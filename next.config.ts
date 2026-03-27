e897f365dfa1d40ac8a68fc826e5d5d867ec6fe7
drdhavaltrivedi <drdhavaltrivedi5@gmail.com>
2026-03-27 17:37:59 +0530

    Troubleshoot 500 error: Revert CSP headers in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     outputFileTracingRoot: __dirname,
-    async headers() {
-        return [
-            {
-                source: "/(.*)",
-                headers: [
-                    {
-                        key: "Content-Security-Policy",
-                        value: "script-src 'self' 'unsafe-eval' 'unsafe-inline'; object-src 'none';",
-                    },
-                ],
-            },
-        ];
-    },
 };
 
 export default nextConfig;
