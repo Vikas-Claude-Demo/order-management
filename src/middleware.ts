d59cb5a509285fc9da47dfb9c6eb519cc3b86202
drdhavaltrivedi <drdhavaltrivedi5@gmail.com>
2026-03-27 17:41:29 +0530

    Troubleshoot 500 error: Passthrough middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,8 @@
-import NextAuth from "next-auth"
-import { authConfig } from "./auth.config"
+import { NextResponse } from "next/server"
 
-export const { auth } = NextAuth(authConfig)
-
-export default auth((req) => {
-  const isLoggedIn = !!req.auth
-  const isOnLogin = req.nextUrl.pathname === "/login"
-
-  if (isOnLogin) {
-    if (isLoggedIn) return Response.redirect(new URL("/", req.nextUrl))
-    return
-  }
-
-  if (!isLoggedIn) {
-    return Response.redirect(new URL("/login", req.nextUrl))
-  }
-})
+export default function middleware() {
+  return NextResponse.next()
+}
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
